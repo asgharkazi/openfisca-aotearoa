7d8fd1900a6232924795496ff8c12ab98c03edb4
Mauko Quiroga <public@mauko.me>
2022-09-25 16:10:21 +0200

    Add parts of nz to area of residence

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         ],
     install_requires = [
         "openfisca-core[web-api] @ git+https://github.com/openfisca/openfisca-core.git@add-weeks",
+        "pandas >= 1.5.0, < 1.6.0",
         ],
     extras_require = {
         "dev": [
